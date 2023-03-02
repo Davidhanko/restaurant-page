ab468754269e6b5957781d9e988ce766ad72fdfa
Davihan11 <davihan11@gmail.com>
2023-03-02 20:44:41 +0100

    Made filler Home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("HELLO!");
+import "./style.css";
+import Logo from "./logo.png";
+const contentDiv = document.querySelector("#content");
+
+function createButtons(text) {}
+
+function displayHome() {
+  // Creating navigation bar
+  let navBar = document.createElement("nav");
+  navBar.classList.add("navigationBar");
+  contentDiv.appendChild(navBar);
+  //creating buttons
+  let button1 = createButtons("Home");
+  let button2 = createButtons("Menu");
+  let button3 = createButtons("Contact");
+}
+
+displayHome();
