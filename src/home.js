ebf62b1a022a3d909af7440ef964590b4787024a
Davihan11 <davihan11@gmail.com>
2023-03-06 18:00:51 +0100

    Home page with JS

---
+++ src/home.js
@@ -0,0 +1,46 @@
+import Logo from "./logo.png";
+
+function createElement(tag, className, text, IMGsource) {
+  let element = document.createElement(`${tag}`);
+  element.classList.add(`${className}`);
+  if (tag !== "img") {
+    element.textContent = `${text}`;
+  } else {
+    element.src = `${IMGsource}`;
+    element.alt = `${text}`;
+  }
+  return element;
+}
+
+export function displayHome() {
+  const contentDiv = document.querySelector("#content");
+  // Creating navigation bar
+  let navBar = createElement("nav", "navigationBar", "");
+  contentDiv.appendChild(navBar);
+  //creating buttons
+  navBar.appendChild(createElement("button", "navButton", "Home"));
+  navBar.appendChild(createElement("button", "navButton", "Menu"));
+  navBar.appendChild(createElement("button", "navButton", "Contact"));
+  //creating headline
+  contentDiv.appendChild(createElement("h1", "headline", "BlackScraper"));
+  //creating image
+  contentDiv.appendChild(createElement("img", "smallImage", "landscape", Logo));
+  //creating text
+  contentDiv.appendChild(
+    createElement(
+      "p",
+      "welcomeText",
+      "Welcome to the BlackScraper, our four-star restaurant!"
+    )
+  );
+  contentDiv.appendChild(
+    createElement(
+      "p",
+      "welcomeText",
+      "We offer the best steaks in the entire country!"
+    )
+  );
+  contentDiv.appendChild(
+    createElement("p", "welcomeText", "Made with passion from 1967!")
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import "./style.css";
-import Logo from "./logo.png";
-const contentDiv = document.querySelector("#content");
-
-function createButtons(text) {}
-
-function displayHome() {
-  // Creating navigation bar
-  let navBar = document.createElement("nav");
-  navBar.classList.add("navigationBar");
-  contentDiv.appendChild(navBar);
-  //creating buttons
-  let button1 = createButtons("Home");
-  let button2 = createButtons("Menu");
-  let button3 = createButtons("Contact");
-}
+import { displayHome } from "./home";
 
 displayHome();
